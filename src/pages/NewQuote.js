5e780b3b0d9ee79d87662487389477763171cc44
sardorboboshov <72418997+sardorboboshov@users.noreply.github.com>
2021-09-15 23:27:24 +0500

    night changes

--- src/pages/NewQuote.js
+++ src/pages/NewQuote.js
@@ -1,5 +1,16 @@
+import { useHistory } from "react-router";
+
+import QuoteForm from "../components/quotes/QuoteForm";
+
 const NewQuote = () => {
-  return <h1>New Quote Page</h1>;
+  const history = useHistory();
+
+  const addQuoteHandler = (quoteData) => {
+    console.log(quoteData);
+
+    history.push("/quotes");
+  };
+  return <QuoteForm onAddQuote={addQuoteHandler} />;
 };
 
 export default NewQuote;
--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,14 +1,24 @@
 import { Fragment } from "react";
 import { useParams, Route } from "react-router";
 import Comments from "../components/comments/Comments";
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+
+const DUMMY_QUOTES = [
+  { id: "q1", author: "Max", text: "Learning React is Fun" },
+  { id: "q2", author: "Sardor", text: "Learning React is Great" },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
 
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No Quote Found</p>;
+  }
   return (
     <Fragment>
-      <h2>Quote Detail Page</h2>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
@@ -17,3 +27,5 @@ const QuoteDetail = () => {
 };
 
 export default QuoteDetail;
+
+//2;Z:+z86QQX,S3v>
